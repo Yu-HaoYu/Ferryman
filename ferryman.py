2717cbd3df71d3f3686d92664738a19bc014c4e1
John.Yu <105951132@qq.com>
2021-08-26 22:24:18 +0800

    updated

--- ferryman.py
+++ ferryman.py
@@ -1,61 +1,75 @@
 #!/usr/bin/python
-import os,datetime,logging,json,re
+import sys, os, datetime, logging, json, re
 import requests
 import yaml
 import docker
 from retry.api import retry_call
 
-
+# Define the log format
 logging.basicConfig(
     level=logging.INFO,
-    #level=logging.DEBUG,
+    # level=logging.DEBUG,
     format='[%(asctime)s] [%(levelname)s] [%(funcName)s] %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
 
-pydocker = docker.APIClient(timeout=60, base_url='unix:///var/run/docker.sock')
+# Create docker client
+docker_client = docker.APIClient(timeout=60, base_url='unix:///var/run/docker.sock')
+
 
 #################################################
 
-# 目标私人仓库认证
-# 优先从环境变量TARGET_USER、TARGET_PASSWORD获取私人仓库账号密码，用于支持GitHub Actions
-if os.environ.get('TARGET_USER') and os.environ.get('TARGET_PASSWORD'):
-    target_user = os.environ.get('TARGET_USER')
-    target_password = os.environ.get('TARGET_PASSWORD')
-    target_auth = { 'username': target_user, 'password': target_password }
-else:
-    target_auth = {'username': 'Your username', 'password': 'Your password'}
+class Utils():
+    def __init__(self):
+        # Init
+        self.__init()
+
+    # Init
+    def __init(self) -> None:
+        self.create_dir("history")
+
+    # Login authentication
+    @staticmethod
+    def auth() -> dict:
+        try:
+            target_user = os.environ.get('TARGET_USER')
+            target_password = os.environ.get('TARGET_PASSWORD')
+            auth = {
+                'username': target_user,
+                'password': target_password
+            }
+        except ValueError:
+            sys.exit(
+                f'ERROR: The environment variable `TARGET_USER` or `TARGET_PASSWORD` cannot be found, please check')
+        return auth
+
+    # Create a directory
+    def create_dir(self, dir: str) -> None:
+        if os.path.exists(dir):
+            logging.info(f"The {dir} directory already exists")
+        else:
+            logging.info(f"No directory found for {dir}")
+            os.makedirs(dir)
+            logging.info(f"Create {dir} directory")
+        return
+
+    # Load the list of synchronized items (YML file)
+    def load_yml(self, file: str) -> dict:
+        logging.info(f"Load YML File: {file}")
+        with open(file, "rb") as f:
+            result = yaml.load(f, Loader=yaml.SafeLoader)
+            logging.debug(f"Load YML File: {result}")
+        return result
 
 
 # 重新封装Docker模块Push方法，增加登录验证失败报错与推送镜像时显示进度条
 def docker_push(image, auth):
-    output = pydocker.push(image, auth_config=auth, stream=True, decode=True)
+    output = docker_client.push(image, auth_config=auth, stream=True, decode=True)
     for line in output:
         if line.get("error"):
             raise InterruptedError(line.get("error").replace('\n', ' '))
         if line.get("progress"):
             print(line.get("status"), line.get("progress"), end="\r")
-    return
-
-
-# 创建目录
-def create_dir(dir_path):
-    if os.path.exists(dir_path):
-        logging.info (f"The {dir_path} directory already exists")
-    else:
-        logging.info (f"No directory found for {dir_path}")
-        os.makedirs(dir_path)
-        logging.info (f"Create {dir_path} directory")
-    return
-
-
-# 加载同步项目清单（YML文件）
-def load_yml(file):
-    logging.info (f"Load YML File: {file}")
-    with open(file, "rb") as f:
-        result = yaml.load(f, Loader=yaml.SafeLoader)
-        logging.debug (f"Load YML File:  {result}")
-    return result
 
 
 # 读取本地历史更新记录
@@ -63,14 +77,14 @@ def load_history(items, domain):
     file = (f"./history/{domain}__{items}.txt")
     result = []
     if os.path.exists(file):
-        logging.debug (f"Discover local history files: {file}")
+        logging.debug(f"Discover local history files: {file}")
         with open(file, 'r', encoding='utf-8') as f:
             for line in f:
                 line = line.strip('\n').split("\t\t")
                 result.append({f"dt": line[0], "sha256": line[1], "tag": line[2]})
-        logging.debug (f"Local List: Count {len(result)}, {result}")
+        logging.debug(f"Local List: Count {len(result)}, {result}")
     else:
-        logging.debug (f"Can't find local history file: {file}")
+        logging.debug(f"Can't find local history file: {file}")
     return result
 
 
@@ -81,7 +95,7 @@ def write_history(items, domain, local_list):
     for i in local_list:
         f.write(f'{i["dt"]}\t\t{i["sha256"]}\t\t{i["tag"]}\n')
     f.close()
-    logging.info ("Append tags to local history")
+    logging.info("Append tags to local history")
     return
 
 
@@ -90,7 +104,7 @@ def datetime_conv(dt):
     dt = str(dt)
     if dt.isdigit():
         if len(dt) > 10:
-            dt = int(dt)/1000
+            dt = int(dt) / 1000
         d = datetime.datetime.fromtimestamp(int(dt))
     else:
         d = datetime.datetime.strptime(dt, '%Y-%m-%dT%H:%M:%S.%fZ')
@@ -99,8 +113,8 @@ def datetime_conv(dt):
 
 
 # 获取k8s的tag
-def requests_gcr(items, namespaces = "none"):
-    logging.info (f"The current mirror source is k8s.gcr.io")
+def requests_gcr(items, namespaces="none"):
+    logging.info(f"The current mirror source is k8s.gcr.io")
     # 获取Tag
     if namespaces == "none":
         url = (f"https://k8s.gcr.io/v2/{items}/tags/list")
@@ -117,13 +131,13 @@ def requests_gcr(items, namespaces = "none"):
             dt = datetime_conv(v[1]["timeUploadedMs"])
             src_list.append({"dt": dt, "sha256": sha256, "tag": tag})
     # 按时间字段排序
-    result = sorted(src_list, key=lambda x: x["dt"],reverse=True)
+    result = sorted(src_list, key=lambda x: x["dt"], reverse=True)
     return result
 
 
 # 获取quay.io的tag
-def requests_quay(items, namespaces = "none"):
-    logging.info (f"The current mirror source is quay.io")
+def requests_quay(items, namespaces="none"):
+    logging.info(f"The current mirror source is quay.io")
     # 循环获取Tag
     response_list = []
     for i in range(100):
@@ -133,11 +147,11 @@ def requests_quay(items, namespaces = "none"):
         buffer_list = r.json().get("tags")
 
         if len(buffer_list) != 0:
-            logging.info (f"The { page } page, tags count {len (buffer_list)}")
+            logging.info(f"The {page} page, tags count {len(buffer_list)}")
             for x in buffer_list:
                 response_list.append(x)
         else:
-            logging.info (f"The { page } page, this is the last page")
+            logging.info(f"The {page} page, this is the last page")
             break
     # 提取Tag、Date、sha256
     src_list = []
@@ -150,13 +164,13 @@ def requests_quay(items, namespaces = "none"):
             dt = datetime_conv(0)
         src_list.append({"dt": dt, "sha256": sha256, "tag": tag})
     # 按时间字段排序
-    result = sorted(src_list, key=lambda x: x["dt"],reverse=True)
+    result = sorted(src_list, key=lambda x: x["dt"], reverse=True)
     return result
 
 
 # 获取dockio.io的tag
-def requests_docker(items, namespaces = "none"):
-    logging.info (f"The current mirror source is docker.io")
+def requests_docker(items, namespaces="none"):
+    logging.info(f"The current mirror source is docker.io")
     # 循环获取Tag
     response_list = []
     for i in range(100):
@@ -166,11 +180,11 @@ def requests_docker(items, namespaces = "none"):
         buffer_list = r.json().get("results")
 
         if len(buffer_list) != 0:
-            logging.info (f"The { page } page, tags count {len (buffer_list)}")
+            logging.info(f"The {page} page, tags count {len(buffer_list)}")
             for x in buffer_list:
                 response_list.append(x)
         else:
-            logging.info  (f"The { page } page, this is the last page")
+            logging.info(f"The {page} page, this is the last page")
             break
     # 提取Tag和Date
     src_list = []
@@ -190,26 +204,28 @@ def requests_docker(items, namespaces = "none"):
                     sha256 = "Unknown"
                 src_list.append({"dt": dt, "sha256": sha256, "tag": tag})
     # 按时间字段排序
-    result = sorted(src_list, key=lambda x: x["dt"],reverse=True)
+    result = sorted(src_list, key=lambda x: x["dt"], reverse=True)
     return result
 
 
 # 定义镜像缓存队列
 queue_list = []
+
+
 # 清理缓存镜像
 def cache_cleanup():
     global queue_list
     for i in range(len(queue_list)):
-        logging.debug (f"Queue count: {len(queue_list)}，Current clean image: {queue_list[0]}")
-        pydocker.remove_image(queue_list[0])
+        logging.debug(f"Queue count: {len(queue_list)}，Current clean image: {queue_list[0]}")
+        docker_client.remove_image(queue_list[0])
         queue_list.pop(0)
         if len(queue_list) == 0:
-            logging.debug (f"Queue count: {len(queue_list)}")
+            logging.debug(f"Queue count: {len(queue_list)}")
     return
 
 
 # 镜像同步
-def sync_images(image, src_repo, target_repo, tag_list):
+def sync_images(image, src_repo, target_repo, target_auth, tag_list):
     try:
         for num, item in enumerate(tag_list, start=1):
             total = len(tag_list)
@@ -222,23 +238,23 @@ def sync_images(image, src_repo, target_repo, tag_list):
             domain = src_repo.split("/")[0]
 
             starttime = datetime.datetime.now()
-            logging.info (f"({num}/{total}) {image}:{tag}")
-            logging.info (f"Image Update: {dt}")
+            logging.info(f"({num}/{total}) {image}:{tag}")
+            logging.info(f"Image Update: {dt}")
 
-            logging.info (f"Pull Image: {image}, Tag: {tag}")
-            retry_call(pydocker.pull, fargs=[source], exceptions=Exception, tries=6, delay=10)
+            logging.info(f"Pull Image: {image}, Tag: {tag}")
+            retry_call(docker_client.pull, fargs=[source], exceptions=Exception, tries=6, delay=10)
 
-            logging.info (f"Tag  Image: {image}, Tag: {tag}")
-            pydocker.tag(source, target)
-            #pydocker.remove_image(source)
+            logging.info(f"Tag  Image: {image}, Tag: {tag}")
+            docker_client.tag(source, target)
+            # pydocker.remove_image(source)
 
-            logging.info (f"Push Image: {image}, Tag: {tag}")
-            retry_call(docker_push, fargs=[target, target_auth] , exceptions=Exception, tries=6, delay=10)
-            pydocker.remove_image(target)
+            logging.info(f"Push Image: {image}, Tag: {tag}")
+            retry_call(docker_push, fargs=[target, target_auth], exceptions=Exception, tries=6, delay=10)
+            docker_client.remove_image(target)
 
             # 加载本地历史同步记录
             local_list = load_history(image, domain)
-            logging.debug (f"History list: Count {len(local_list)}, {local_list}")
+            logging.debug(f"History list: Count {len(local_list)}, {local_list}")
             # 以当前Tag去重后排序回写
             local_list = [i for i in local_list if tag != i["tag"]]
             local_list.append({"dt": dt, "sha256": sha256, "tag": tag})
@@ -249,31 +265,33 @@ def sync_images(image, src_repo, target_repo, tag_list):
             global queue_list
             queue_list.append(source)
             if len(queue_list) == 6:
-                logging.debug (f"Count before clearing the queue: {len(queue_list)}，Queue list: {queue_list}")
-                logging.debug (f"Current clean image: {queue_list[0]}")
-                pydocker.remove_image(queue_list[0])
+                logging.debug(f"Count before clearing the queue: {len(queue_list)}，Queue list: {queue_list}")
+                logging.debug(f"Current clean image: {queue_list[0]}")
+                docker_client.remove_image(queue_list[0])
                 queue_list.pop(0)
-                logging.debug (f"Count after clearing the queue: {len(queue_list)}，Queue list: {queue_list}")
+                logging.debug(f"Count after clearing the queue: {len(queue_list)}，Queue list: {queue_list}")
 
             endtime = datetime.datetime.now()
-            logging.info ("Execution time: %s\n" %(endtime - starttime))
+            logging.info("Execution time: %s\n" % (endtime - starttime))
     except (Exception, KeyboardInterrupt) as e:
         # 缓存镜像：结束前删除队列中剩余镜像
         cache_cleanup()
 
-        logging.error (e)
-        logging.warning ("%s Sync Failed, Abnormal Exit!" % image)
+        logging.error(e)
+        logging.warning("%s Sync Failed, Abnormal Exit!" % image)
         exit(1)
     else:
         # 缓存镜像：结束前删除队列中剩余镜像
         cache_cleanup()
 
-        logging.info ("%s Sync Success" % image)
+        logging.info("%s Sync Success" % image)
     return
 
 
 # 主逻辑
 def main(yml):
+    target_auth = Utils.auth()
+
     # 获取同步项目清单
     for (key, value) in yml.items():
         items = key
@@ -282,10 +300,10 @@ def main(yml):
         namespaces = source.split("/")[-2]
         target = value["target"]
         limit = value["tag"]["limit"]
-        
+
         # 开始同步
-        logging.info (f"Start syncing: {items}")
-        logging.info (f"Update limit: {limit}")
+        logging.info(f"Start syncing: {items}")
+        logging.info(f"Update limit: {limit}")
 
         # 判断源
         if "k8s.gcr.io" in source:
@@ -298,26 +316,26 @@ def main(yml):
         elif 'docker.io' in source:
             src_list = requests_docker(items, namespaces)
         else:
-            logging.info ("Unsupported sync source")
-            exit ()
+            logging.info("Unsupported sync source")
+            exit()
 
         # 排除掉Win平台、Gitlab-ce nightly
         new_src_list = src_list.copy()
         for i in new_src_list:
-            if "windowsservercore" in str(i) or "nanoserver" in str(i) or "nightly" in str(i)  or "windows" in str(i):
+            if "windowsservercore" in str(i) or "nanoserver" in str(i) or "nightly" in str(i) or "windows" in str(i):
                 src_list.remove(i)
         new_src_list.clear()
 
         # 限制更新Tag数量
         src_list = src_list[:limit]
 
-        logging.info (f"Latest version: {src_list[0]['tag']}, Lastst updated: {src_list[0]['dt']}")
-        logging.debug (f"Src list: Count {len(src_list)}, {src_list}")
+        logging.info(f"Latest version: {src_list[0]['tag']}, Lastst updated: {src_list[0]['dt']}")
+        logging.debug(f"Src list: Count {len(src_list)}, {src_list}")
 
         # 加载本地同步记录
         history_list = load_history(items, domain)
 
-        logging.debug (f"History list: Count {len(history_list)}, {history_list}")
+        logging.debug(f"History list: Count {len(history_list)}, {history_list}")
 
         # 递归获取源列表与本地列表差集
         dedupl_list = src_list.copy()
@@ -329,33 +347,26 @@ def main(yml):
         # 按时间字段排序（降序）
         dedupl_list.sort(reverse=True, key=lambda x: x['dt'])
 
-        logging.debug (f"Sync list: Count {len(dedupl_list)}, {dedupl_list}")
+        logging.debug(f"Sync list: Count {len(dedupl_list)}, {dedupl_list}")
         # 如果同步列表为空则不同步
         if not dedupl_list:
-            logging.info (f"Src List: {len(src_list)}, History List: {len(history_list)}, Sync List: {len(dedupl_list)}")
-            logging.info (f"The Sync list of the {items} is empty, no need to sync.")
-            logging.info (f"{items} Sync Success")
+            logging.info(f"Src List: {len(src_list)}, History List: {len(history_list)}, Sync List: {len(dedupl_list)}")
+            logging.info(f"The Sync list of the {items} is empty, no need to sync.")
+            logging.info(f"{items} Sync Success")
         else:
-            logging.info (f"Src List: {len(src_list)}, History List: {len(history_list)}, Sync List: {len(dedupl_list)}")
+            logging.info(f"Src List: {len(src_list)}, History List: {len(history_list)}, Sync List: {len(dedupl_list)}")
             # 调用镜像同步
             for i in items.splitlines(True):
-                sync_images(items, source, target, dedupl_list)
-        print ()
+                sync_images(items, source, target, target_auth, dedupl_list)
+        print()
     return
 
 
-# 初始化
-def init():
-    logging.info ("Start synchronization")
-
-    create_dir("history")
-    yml_file = load_yml("items.yml")
-    print ()
-    main(yml_file)
-
-
-# 入口
+# main
 if __name__ == "__main__":
-    init()
+    logging.info("Start synchronization")
 
+    utils = Utils()
+    config = utils.load_yml("items.yml")
 
+    main(config)
